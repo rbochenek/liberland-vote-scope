a07f53137704238d2f312b23c545c26c2e792f8b
Radek Bochenek <radekb@mac.com>
2025-03-22 00:03:04 +0100

    feat: bind HTTP server to all interfaces

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ async fn main() -> std::io::Result<()> {
     // Start HTTP server
     HttpServer::new(|| App::new().service(election))
         .workers(3)
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
